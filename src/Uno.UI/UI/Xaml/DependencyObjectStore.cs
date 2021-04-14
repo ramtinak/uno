d071c84b97515d81c235c89d8e1d49cb8c9c0d8f
Jerome Laban <jerome.laban@nventive.com>
2021-04-14 16:05:14 -0400

    chore: adjust sources for net6 android/ios build

--- src/Uno.UI/UI/Xaml/DependencyObjectStore.cs
+++ src/Uno.UI/UI/Xaml/DependencyObjectStore.cs
@@ -1826,7 +1826,7 @@ namespace Windows.UI.Xaml
 				=> Instance.GetHashCode() ^
 					Property.UniqueId.GetHashCode();
 
-			public override bool Equals(object obj)
+			public override bool Equals(object? obj)
 				=> Equals(obj as DependencyPropertyPath);
 
 			public bool Equals(DependencyPropertyPath? other)
@@ -1857,7 +1857,7 @@ namespace Windows.UI.Xaml
 		{
 			public static readonly WeakReferenceValueComparer Default = new WeakReferenceValueComparer();
 
-			public bool Equals(WeakReference x, WeakReference y)
+			public bool Equals(WeakReference? x, WeakReference? y)
 			{
 				return ReferenceEquals(x?.Target, y?.Target);
 			}
